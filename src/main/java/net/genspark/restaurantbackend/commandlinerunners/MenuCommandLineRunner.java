4d6b07cf528b6f41de5a05c6bb7ef982b0ede446
Kevin Schoen <kevschoen83@gmail.com>
2022-06-16 10:51:27 -0500

    Added menu images

--- src/main/java/net/genspark/restaurantbackend/commandlinerunners/MenuCommandLineRunner.java
+++ src/main/java/net/genspark/restaurantbackend/commandlinerunners/MenuCommandLineRunner.java
@@ -48,26 +48,26 @@ public class MenuCommandLineRunner implements CommandLineRunner {
                 5.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
 
         items.add(new MenuItem(4, "Pizza", "Supreme", "14-inch with pepperoni, sausage, ham, onion, mushroom, and bell pepper",
-                14.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                14.99, "https://restaurantclicks.com/wp-content/uploads/2021/09/tasty-supreme-pizza-with-olives-peppers-onions-and-sausage.jpg"));
         items.add(new MenuItem(5, "Pizza", "Pepperoni", "14-inch with double pepperoni",
-                12.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                12.99, "https://cdn.vox-cdn.com/thumbor/fHcMf9a4S7iaY2zakdIENd2yXYA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/10235609/pepperoni.jpeg"));
         items.add(new MenuItem(6, "Pizza", "Cheese", "14-inch with extra cheese",
-                10.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                10.99, "https://lh3.googleusercontent.com/-1Iil24qU-EM/VqnJ50T5vII/AAAAAAAFQzI/O2jXEMQXKt0/s800-Ic42/cheese-pizza-2.jpg"));
 
         items.add(new MenuItem(7, "Sides", "French Fries", "Small cup of fresh, golden-brown french fries",
-                1.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                1.99, "https://upload.wikimedia.org/wikipedia/commons/8/83/French_Fries.JPG"));
         items.add(new MenuItem(8, "Sides", "Salad", "Small Bowl - Caesar Salad",
-                1.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                1.99, "https://www.jessicagavin.com/wp-content/uploads/2019/07/caesar-salad-10-1200.jpg"));
         items.add(new MenuItem(9, "Sides", "Baked Potato", "Loaded Baked Potato with cheese, bacon bits, and sour cream",
-                1.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                1.99, "https://www.orwhateveryoudo.com/wp-content/uploads/2020/07/Smoked-Loaded-Baked-Potato-12-720x720.jpg"));
 
         items.add(new MenuItem(13, "Dessert", "Raspberry Cheesecake", "Slice of cheesecake with raspberry topping",
-                5.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                5.99, "https://bakingamoment.com/wp-content/uploads/2019/08/white-chocolate-raspberry-cheesecake.jpg"));
         items.add(new MenuItem(14, "Dessert", "Dessert Pizza", "5-inch pizza with cinnamon, chocolate chips, and whipped cream",
-                5.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                5.99, "https://sugarspunrun.com/wp-content/uploads/2018/04/Chocolate-Chip-Dessert-Pizza-Recipe-1-of-1-8.jpg"));
         items.add(new MenuItem(15, "Dessert", "Ice Cream Sundae", "3-scoops of vanilla ice cream with chocolate syrup, " +
                 "whipped cream, and 1 cherry on top",
-                4.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+                4.99, "https://izzycooking.com/wp-content/uploads/2021/05/Ice-Cream-Sundae-thumbnail.jpg"));
 
         return items;
     }
