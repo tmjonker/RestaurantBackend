f84b594181f5786f1886cb96626b29d531bfdaf6
Kevin Schoen <kevschoen83@gmail.com>
2022-06-16 08:33:23 -0500

    Added extra menu items

--- src/main/java/net/genspark/restaurantbackend/commandlinerunners/MenuCommandLineRunner.java
+++ src/main/java/net/genspark/restaurantbackend/commandlinerunners/MenuCommandLineRunner.java
@@ -39,12 +39,35 @@ public class MenuCommandLineRunner implements CommandLineRunner {
                 7.99, "https://static.onecms.io/wp-content/uploads/sites/43/2022/04/07/23596-fried-mozzarella-cheese-sticks-3x2-252.jpg"));
         items.add(new MenuItem(12, "Appetizer", "Boneless Wings", "Boneless wings with ranch or bleu cheese",
                 8.99, "https://lilluna.com/wp-content/uploads/2018/05/honey-bbq-wings-resize-5.jpg"));
+
         items.add(new MenuItem(1, "Burger", "Double Cheeseburger", "Two beef patties with American cheese",
                 3.99, "https://www.carlsjr.com/-/media/project/cke/carlsjr/products/charboiled-burgers/cj_site_705x742_doublecheeseburger.jpg"));
         items.add(new MenuItem(2, "Burger", "Cheeseburger", "One beef patty with American cheese",
                 2.49, "https://www.hamburgerstand.com/wp-content/uploads/2015/03/burgers-cheeseburger-main.jpg"));
         items.add(new MenuItem(3, "Burger", "Triple Stack", "Three beef patties with American cheese",
                 5.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+
+        items.add(new MenuItem(4, "Pizza", "Supreme", "14-inch with pepperoni, sausage, ham, onion, mushroom, and bell pepper",
+                14.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+        items.add(new MenuItem(5, "Pizza", "Pepperoni", "14-inch with double pepperoni",
+                12.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+        items.add(new MenuItem(6, "Pizza", "Cheese", "14-inch with extra cheese",
+                10.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+
+        items.add(new MenuItem(7, "Sides", "French Fries", "Small cup of fresh, golden-brown french fries",
+                1.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+        items.add(new MenuItem(8, "Sides", "Salad", "Small Bowl - Caesar Salad",
+                1.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+        items.add(new MenuItem(9, "Sides", "Baked Potato", "Loaded Baked Potato with cheese, bacon bits, and sour cream",
+                1.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+
+        items.add(new MenuItem(13, "Dessert", "Raspberry Cheesecake", "Slice of cheesecake with raspberry topping",
+                5.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+        items.add(new MenuItem(14, "Dessert", "Dessert Pizza", "5-inch pizza with cinnamon, chocolate chips, and whipped cream",
+                5.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
+        items.add(new MenuItem(15, "Dessert", "Ice Cream Sundae", "3-scoops of vanilla ice cream with chocolate syrup, " +
+                "whipped cream, and 1 cherry on top",
+                4.99, "https://images.fineartamerica.com/images-medium-large-5/large-beef-burger-with-cheese-and-salad-mark-sykes.jpg"));
 
         return items;
     }
